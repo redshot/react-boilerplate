9dae74f3e5be3ea6b870028e10e26dc37b1bc16e
redshot <adrianberenguel90@gmail.com>
2020-11-08 01:34:26 +0800

    Configured webpack to load or transform js files through babel

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@ import getGreeting from './greet'
 
 // Files in bin directory are executable
 // By default webpack is gonna create a dist directory, where output will go
-// We need a compiler/transpiler as part of the process to help transform new javascript code that will run on older browsers
+// We need a compiler/transpiler as part of the process to help transform modern javascript code that will run on older browsers
 console.log(getGreeting('world'));
--- webpack.config.js
+++ webpack.config.js
@@ -3,10 +3,23 @@ const path = require('path')
 // Set module.exports and output property to an object
 // Webpack will automatically pickup the config file base on its name alone and decide the settings to how to output the bundle
 // development is unminified while producttion mode is minified. if there is no mode indicated webpack will minify it
+// module property takes an object with rules property array that contains an object. each item in rules array can be an object
 module.exports = {
 	entry: './src/index.js',
 	output: {
 		path: path.join(__dirname, 'dist'),
 		filename: 'app.bundle.js'
-	}
+	},
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        options: {
+          presets: ['@babel/preset-env']
+        }
+      }
+    ]
+  }	
 }
