ea2b0fa1d85e5b98b3afba12572a9591f58ae0f8
redshot <adrianberenguel90@gmail.com>
2020-11-08 01:22:31 +0800

    Installed babel

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import greeting from './greet'
-
+import getGreeting from './greet'
 
 // Files in bin directory are executable
 // By default webpack is gonna create a dist directory, where output will go
-console.log("Hello world");
+// We need a compiler/transpiler as part of the process to help transform new javascript code that will run on older browsers
+console.log(getGreeting('world'));
