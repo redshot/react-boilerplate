4cad44f3467ef8ce98f4ed8b9b836023a3055a41
redshot <adrianberenguel90@gmail.com>
2020-11-08 00:51:15 +0800

    Control webpack output with mode setting

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path')
 
 // Set module.exports and output property to an object
 // Webpack will automatically pickup the config file base on its name alone and decide the settings to how to output the bundle
+// development is unminified while producttion mode is minified. if there is no mode indicated webpack will minify it
 module.exports = {
 	entry: './src/index.js',
 	output: {
