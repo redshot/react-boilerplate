9a167f1d50791f4a8254aa2500df35ae45db646e
redshot <adrianberenguel90@gmail.com>
2020-11-07 23:57:10 +0800

    Installed webpack

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import greeting from './greet'
+
+
+// Files in bin directory are executable
+// By default webpack is gonna create a dist directory, where output will go
+console.log("Hello world");
