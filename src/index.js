20bfcb33669b7ac29e00d59e18392f75ebbc31e0
redshot <adrianberenguel90@gmail.com>
2020-11-08 08:25:20 +0800

    Configured babel for React with preset-react

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
-import getGreeting from './greet'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+
+// put component into html page
+ReactDOM.render(<App />, document.getElementById('app'));
 
 // Files in bin directory are executable
 // By default webpack is gonna create a dist directory, where output will go
 // We need a compiler/transpiler as part of the process to help transform modern javascript code that will run on older browsers
-console.log(getGreeting('world'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,5 @@
 const path = require('path')
 
-// Set module.exports and output property to an object
-// Webpack will automatically pickup the config file base on its name alone and decide the settings to how to output the bundle
-// development is unminified while producttion mode is minified. if there is no mode indicated webpack will minify it
-// module property takes an object with rules property array that contains an object. each item in rules array can be an object
 module.exports = {
 	entry: './src/index.js',
 	output: {
@@ -17,9 +13,15 @@ module.exports = {
         loader: 'babel-loader',
         exclude: /node_modules/,
         options: {
-          presets: ['@babel/preset-env']
+          presets: ['@babel/preset-env', '@babel/preset-react']
         }
       }
     ]
   }	
 }
+
+// Set module.exports and output property to an object
+// Webpack will automatically pickup the config file base on its name alone and decide the settings to how to output the bundle
+// development is unminified while producttion mode is minified. if there is no mode indicated webpack will minify it
+// module property takes an object with rules property array that contains an object. each item in rules array can be an object
+// Without @babel/preset-react, npm run build will display an error because it does not know how to handle JSX. JSX is not a valid JavaScript
