882238b9f46be67dcab5c93b5b8c54788e94e303
redshot <adrianberenguel90@gmail.com>
2020-11-15 01:57:55 +0800

    Generated source map through webpack for better debugging

--- webpack.config.base.js
+++ webpack.config.base.js
@@ -24,13 +24,16 @@ module.exports = {
   })]
 }
 
-// Set module.exports and output property to an object
-// Webpack will automatically pickup the config file base on its name alone and decide the settings to how to output the bundle
-// development is unminified while producttion mode is minified. if there is no mode indicated webpack will minify it
-// module property takes an object with rules property array that contains an object. each item in rules array can be an object
-// Without @babel/preset-react, npm run build will display an error because it does not know how to handle JSX. JSX is not a valid JavaScript
-// The HtmlWebpackPlugin simplifies creation of HTML files to serve your webpack bundles.
-// Our webpack configuration is composed of 3 files so we have 1 place where we put all share settings. 
-//  Additionally, we have separate file where we can only pud dev mode settings or production mode setting
-// npm i -D name-of-package save the package as devdependency
-// webpack-dev-server watches the files and will do the build in the dist folder then send the signal to the browser to reload the page
+/**
+ * Set module.exports and output property to an object
+ * Webpack will automatically pickup the config file base on its name alone and decide the settings to how to output the bundle
+ * development is unminified while producttion mode is minified. if there is no mode indicated webpack will minify it
+ * module property takes an object with rules property array that contains an object. each item in rules array can be an object
+ * Without @babel/preset-react, npm run build will display an error because it does not know how to handle JSX. JSX is not a valid JavaScript
+ * The HtmlWebpackPlugin simplifies creation of HTML files to serve your webpack bundles.
+ * Our webpack configuration is composed of 3 files so we have 1 place where we put all share settings. 
+ *  Additionally, we have separate file where we can only pud dev mode settings or production mode setting
+ * npm i -D name-of-package save the package as devdependency
+ * webpack-dev-server watches the files and will do the build in the dist folder then send the signal to the browser to reload the page
+ * 
+ */
--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -5,5 +5,11 @@ module.exports = merge(baseConfig, {
   mode: 'development',
   devServer: {
     port: 9000
-  }
+  },
+  devtool: 'source-map'
 })
+
+/**
+ * Source map is gonna map the source code to the actual code running on the browser
+ * 
+ */
