bf6e89bb35a103268648b88c1c3e8f7dd1f47060
redshot <adrianberenguel90@gmail.com>
2020-11-16 08:18:03 +0800

    Analyzed javascript bundle through webpack bundle analyzer

--- webpack.config.base.js
+++ webpack.config.base.js
@@ -35,7 +35,7 @@ module.exports = {
 
 /**
  * Set module.exports and output property to an object
- * The command to run the project in npm run dev or depending on the config in package.json
+ * The command to run the project in npm run dev, npm run build or depending on the config in package.json
  * Webpack will automatically pickup the config file base on its name alone and decide the settings to how to output the bundle
  * development is unminified while producttion mode is minified. if there is no mode indicated webpack will minify it
  * module property takes an object with rules property array that contains an object. each item in rules array can be an object
@@ -43,11 +43,14 @@ module.exports = {
  * The HtmlWebpackPlugin simplifies creation of HTML files to serve your webpack bundles.
  * Our webpack configuration is composed of 3 files so we have 1 place where we put all share settings. 
  *  Additionally, we have separate file where we can only pud dev mode settings or production mode setting
- * npm i -D name-of-package save the package as devdependency
+ * npm i -D name-of-package saves the package as devdependency
  * webpack-dev-server watches the files and will do the build in the dist folder then send the signal to the browser to reload the page
  * We need plugin-proposal-class-properties package to understand proposed javascript features syntax like state = { count: 0 }.
  * test: /\.js$/ and test: /\.js$/ contains regular expression test for js and css extensions
  * css-loader is gonna allow webpack to handle css syntax while style-loader is gonna take the css and inject the style tag into html runtime
  * react-hot-loader can update the part of the page without reloading the whole page
+ * As the app gets more complex, we will be able to keep an eye of the size of our bundle through webpack-bundle-analyzer 
+ * 
+ * 
  * 
  */
