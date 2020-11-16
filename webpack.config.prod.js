6b466b319a0aaa10f819e371909c616707ec635c
redshot <adrianberenguel90@gmail.com>
2020-11-16 09:11:34 +0800

    Externalized dependencies to be loaded via cdn

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -8,5 +8,18 @@ module.exports = merge(baseConfig, {
     analyzerMode: 'static',
     openAnalyzer: false,
     reportFilename: 'bundle_sizes.html'
-  })]
+  })],
+  externals: {
+    react: 'React',
+    'react-dom': 'ReactDOM'
+  }
 })
+
+/**
+ * Webpack is gonna include the React and ReactDOM libraries as part of the bundle every time we run our build
+ * For our production build, we will externalize React and ReactDOM libraries and pull them from a CDN
+ * externals is a key that's gonna take an object
+ * We have the ability to variables and condition in our index.html file because of html webpack plugin
+ * 
+ * 
+ */
